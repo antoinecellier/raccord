b77b4c1328bb2d599080f56c4d4f4115bbd49a0f
Hugo Wood <hugo.wood-ext@vif.fr>
2016-07-25 10:56:45 +0200

    refactor: simpler Falcor server start code

--- server/falcor/bin/server.js
+++ server/falcor/bin/server.js
@@ -1,23 +1,8 @@
-var fs = require('fs');
+const fs = require('fs');
 
-var babelrc = fs.readFileSync('./.babelrc');
-var serverConfig = fs.readFileSync('../config.json');
-var babelConfig, falcorServerConfig;
-
-try {
-  babelConfig = JSON.parse(babelrc);
-} catch (err) {
-  console.error('==>     ERROR: Error parsing your .babelrc.');
-  console.error(err);
-}
-
-try {
-  falcorServerConfig = JSON.parse(serverConfig);
-} catch (err) {
-  console.error('==>     ERROR: Error parsing your config.json .');
-  console.error(err);
-}
+const babelConfig = JSON.parse(fs.readFileSync('./.babelrc'));
+const serverConfig = require('../../config');
 
 require('babel-core/register')(babelConfig);
-var FalcorServer = require('../server').FalcorServer;
-new FalcorServer(falcorServerConfig.falcor.port)
+const falcorServer = require('../server').default;
+falcorServer(serverConfig.falcor.port);

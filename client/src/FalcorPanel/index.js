f24d42b3175c6b6f62522ffca60be051d55d5448
antoine_cellier <antoine08.cellier@hotmail.fr>
2016-08-24 22:04:06 +0200

    add falcor request editor

--- client/src/FalcorPanel/index.js
+++ client/src/FalcorPanel/index.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import AceEditor from 'react-ace';
+import 'brace/mode/javascript';
+import 'brace/theme/github';
 
 export default class FalcorPanel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      request: ''
+    }
+    this.handleRequest = request => this.setState({ request });
+  }
+
   render() {
+    console.log(this.state.request);
     return (
       <div className="col-md-6">
         Falcor
+        <AceEditor
+          mode="javascript"
+          theme="github"
+          name="falcor_editor"
+          fontSize={15}
+          height={200}
+          onChange={this.handleRequest}
+          value={this.state.request}
+          editorProps={{$blockScrolling: true}}
+        />
       </div>
     );
   }

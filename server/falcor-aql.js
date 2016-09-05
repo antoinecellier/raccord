f27e23ffcc48cd271533cef634e37b7e3b6d11ed
Hugo Wood <hx4.5@free.fr>
2016-09-06 00:49:04 +0200

    wip

--- server/falcor-aql.js
+++ server/falcor-aql.js
@@ -1,106 +1,113 @@
-import parseRoutes from 'falcor-router/src/parse-tree'
-import createMatcher from 'falcor-router/src/operations/matcher'
-import getExecutablesMatches from 'falcor-router/src/run/precedence/getExecutableMatches'
+import FalcorRouter from 'falcor-router'
 import {dataSourceRoute as falcor} from 'falcor-express'
+import ranges from 'falcor-router/src/operations/ranges/convertPathKeyToRange'
 import db from './db'
 import aqb from 'aqb'
 import express from 'express'
 import bodyParser from 'body-parser'
 import co from 'co'
-import sortBy from 'lodash.sortBy'
 import {Observable} from 'rx'
-import merge from 'lodash.merge'
-import shortid from 'shortid'
-import set from 'lodash.set'
-
-const stationPropertyMapping = {
-  id: 'stop_id',
-  name: 'stop_name',
-  longitude: 'stop_lon',
-  latitude: 'stop_lat'
-}
-
-const routePropertyMapping = {
-  id: 'route_id',
-  label: 'route_short_name',
-  description: 'route_long_name'
-}
+import _ from 'lodash'
 
 const routes = [
   {
     route: 'stations.byId[{keys:ids}][{keys:props}]',
     get: function ([stations, byId, ids, props]) {
-      const propertyOf = prop => aqb.ref('station').get(aqb.str(stationPropertyMapping[prop]))
-      const selector = props.reduce((selector, prop) => Object.assign(selector, {[stationPropertyMapping[prop]]: propertyOf(prop)}), {[stationPropertyMapping['id']]: propertyOf('id')})
       const query = aqb.for('station').in('stops')
         .filter(aqb.eq('station.location_type', 1))
         .filter(aqb.in('station.stop_id', aqb.list(ids.map(stationDbId).map(aqb.str))))
-        .return(aqb.obj(selector))
-      console.log('route query', query.toAQL())
-      const mapper = stations => {
-        const dtos = stations.map(stationDto)
-        return {
-          stations: {
-            byId: dtos.reduce((stationsById, station) => Object.assign(stationsById, {[station.id]: station}), {})
-          }
-        }
-      }
-      return {query, mapper}
+        .return('station')
+      const mapper = stations => stations
+        .map(stationDto)
+        .reduce((stationsById, station) => _.assign(stationsById, {[station.id]: _.pick(station, props)}), {})
+      return [{
+        path: [stations, byId],
+        value: {$type: 'aql', query, mapper, paths: [ids, props]}
+      }]
     }
   },
   {
     route: 'routes.byId[{keys:ids}][{keys:props}]',
     get: function ([routes, byId, ids, props]) {
-      const propertyOf = prop => aqb.ref('route').get(aqb.str(routePropertyMapping[prop]))
-      const selector = props.reduce((selector, prop) => Object.assign(selector, {[routePropertyMapping[prop]]: propertyOf(prop)}), {[routePropertyMapping['id']]: propertyOf('id')})
       const query = aqb.for('route').in('routes')
-        .filter(aqb.in('route.stop_id', aqb.list(ids.map(routeDbId).map(aqb.str))))
-        .return(aqb.obj(selector))
-      console.log('route query', query.toAQL())
-      const mapper = routes => {
-        const dtos = routes.map(routeDto)
-        return {
-          routes: {
-            byId: dtos.reduce((routesById, route) => Object.assign(routesById, {[route.id]: route}), {})
-          }
-        }
-      }
-      return {query, mapper}
+        .filter(aqb.in('route.route_id', aqb.list(ids.map(routeDbId).map(aqb.str))))
+        .return('route')
+      const mapper = routes => routes
+        .map(routeDto)
+        .reduce((routesById, route) => _.assign(routesById, {[route.id]: _.pick(route, props)}), {})
+      return [{
+        path: [routes, byId],
+        value: {$type: 'aql', query, mapper, paths: [ids, props]}
+      }]
+    }
+  },
+  {
+    route: 'stations.byId[{keys:ids}].routes[{keys}]',
+    get: function ([stations, byId, ids, routes, keys]) {
+      const [{from, to} = {from: 0, to: -1}] = ranges(keys)
+      const query = aqb.for('station_id').in(aqb.list(ids.map(stationDbId)))
+        .let('children_stops',
+          aqb.for('stop').in('stops')
+          .filter(aqb.eq('stop.parent_station', aqb.ref('station_id')))
+          .return('stop.stop_id'))
+        .let('connected_trips',
+          aqb.for('stop_time').in('stop_times')
+          .filter(aqb.in('stop_time.stop_id', 'children_stops'))
+          .return('stop_time.trip_id'))
+        .let('connected_routes',
+          aqb.for('trip').in('trips')
+          .filter(aqb.in('trip.trip_id', 'connected_trips'))
+          .returnDistinct('trip.route_id'))
+        .let('route_ids',
+          aqb.for('route_id').in('connected_routes')
+          .sort('route_id', 'asc')
+          .limit(from, to - from + 1)
+          .return('route_id'))
+        .return(aqb.obj({
+          stationId: 'station_id',
+          routeIds: 'route_ids',
+          routeCount: aqb.fn('length')('connected_routes')
+        }))
+
     }
   }
 ]
 
 export default express.Router()
   .use(bodyParser.urlencoded({extended: false}))
-  .use('/', falcor(() => aqlDataSource(routes)))
+  .use('/', falcor(() => aqlDataSource(new FalcorRouter(routes))))
 
-function aqlDataSource (routes) {
-  const matcher = createMatcher(parseRoutes(routes))
-  const rxify = fn => (...args) => Observable.fromPromise(co.wrap(aqlGet.bind(null, matcher))(...args).catch(console.error))
-  return {get: rxify(aqlGet)}
-}
+function aqlDataSource (router) {
+  function get (paths) {
+    return router.get(paths).flatMap(jsonGraph => {
+      return Observable.fromPromise(co(function* () {
+        const queries = _(collect(jsonGraph, {$type: 'aql'}))
+          .map(query => _.assign(query, {id: uniqueId()}))
+          .keyBy('id')
+          .value()
+        const bindings = _.reduce(queries, (bindings, {id, query}) => bindings.let(id, query), aqb)
+        const selector = _.mapValues(queries, 'id')
+        const query = bindings.return(aqb.obj(selector))
+        const cursor = yield db().query(query)
+        const dbResults = yield cursor.next()
+        return _.cloneDeepWith(jsonGraph, value => {
+          if (value.$type === 'aql') return value.mapper(dbResults[value.id])
+        })
+      })).doOnError(console.error)
+    })
+  }
+  return {get}
 
-function* aqlGet (matcher, paths) {
-  const matches = sortBy(matcher('get', paths[0]), 'precedence')
-  const {matchAndPaths, unhandledPaths} = getExecutablesMatches(matches, paths)
-  const actionResults = matchAndPaths.map(({match, path}) => match.action(path))
-  const keys = actionResults.map(() => generateKey())
-  const bindings = actionResults.reduce((bindings, {query}, index) => bindings.let(keys[index], query), aqb)
-  const query = bindings.return(aqb.list(keys))
-  const cursor = yield db().query(query)
-  const dbResults = yield cursor.next()
-  const jsonGraphHandled = keys.reduce((jsonGraph, key, index) => {
-    const dbResult = dbResults[index]
-    const {mapper} = actionResults[index]
-    return merge(jsonGraph, mapper(dbResult))
-  }, {})
-  const jsonGraphUnhandled = (unhandledPaths || []).reduce((jsonGraph, path) => {
-    return set(jsonGraph, path, {$type: 'error', value: 'field does not exist'})
-  }, {})
-  return {jsonGraph: merge(jsonGraphHandled, jsonGraphUnhandled)}
+  function uniqueId () {
+    return _.uniqueId('x') // prefix with a letter to make it a valid AQL identifier
+  }
 
-  function generateKey () {
-    return 'x' + shortid.generate().replace('-', '')
+  function collect (obj, predicate) {
+    const results = []
+    _.cloneDeepWith(obj, value => {
+      if (_.iteratee(predicate)(value)) results.push(value)
+    })
+    return results
   }
 }
 

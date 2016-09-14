e9e7fbf337847735a0ab0c30a2bbc6f50c79ba4e
Hugo Wood <hx4.5@free.fr>
2016-09-14 17:07:56 +0200

    Server Falcor pour le second besoin (#11)
    * feat: falcor routes for the second requirement
    
    * fix: list of routes inside stations properly ordered
    
    * feat: respect ranges for stations.routes
    
    * feat: stations.routes.length so routes can be passed through Array.from
    
    * perf: merged routes[ranges] and routes.length falcor routes so that only one db query is made
    
    * refactor: use AQL's return distinct instead of unique()
    
    * fix: correct calculation of stations.routes.length
    
    * feat: coherent names of dto fields

--- server/falcor.js
+++ server/falcor.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import {dataSourceRoute as falcor} from 'falcor-express'
 import FalcorRouter from 'falcor-router'
+import ranges from 'falcor-router/src/operations/ranges/convertPathKeyToRange'
 import flatten from 'lodash.flatten'
 import co from 'co'
 import db, {aql} from './db'
@@ -31,6 +32,47 @@ const routes = [
     })
   },
   {
+    route: 'stations.byId[{keys:ids}].routes[{keys}]',
+    get: co.wrap(function* ([stations, byId, ids, routes, keys]) {
+      const [{from, to} = {from: 0, to: -1}] = ranges(keys)
+      const cursor = yield db().query(aql`
+        for station_id in ${ids.map(stationDbId)}
+          let children_stops = (
+            for stop in stops
+            filter stop.parent_station == station_id
+            return stop.stop_id)
+
+          let connected_trips = (
+            for stop_time in stop_times
+            filter stop_time.stop_id in children_stops
+            return stop_time.trip_id)
+
+          let connected_routes = (
+            for trip in trips
+            filter trip.trip_id in connected_trips
+            return distinct trip.route_id)
+
+          let route_ids = (
+            for route_id in connected_routes
+            sort route_id asc
+            limit ${from}, ${to - from + 1}
+            return route_id)
+
+          return {stationId: station_id, routeIds: route_ids, routeCount: length(connected_routes)}
+      `)
+      return flatten(yield cursor.map(({stationId, routeIds, routeCount}) => routeIds.map((routeId, index) => ({
+        path: [stations, byId, stationDtoId(stationId), routes, index],
+        value: {$type: 'ref', value: [routes, byId, routeDtoId(routeId)]}
+      })).concat({
+        path: [stations, byId, stationDtoId(stationId), routes, 'length'],
+        value: routeCount
+      }).concat(Array(Math.max(0, to - routeIds.length + 1)).fill().map((zero, index) => ({
+        path: [stations, byId, stationDtoId(stationId), routes, routeIds.length + index],
+        value: {$type: 'atom'}
+      })))))
+    })
+  },
+  {
     route: 'stations.byId[{keys:ids}][{keys:props}]',
     get: co.wrap(function* ([stations, byId, ids, props]) {
       const cursor = yield db().query(aql`
@@ -44,13 +86,26 @@ const routes = [
         value: defaultTo(station[prop], {$type: 'error', value: 'field does not exist'})
       }))))
     })
+  },
+  {
+    route: 'routes.byId[{keys:ids}][{keys:props}]',
+    get: co.wrap(function* ([routes, byId, ids, props]) {
+      const cursor = yield db().query(aql`
+        for route in routes
+        filter route.route_id in ${ids.map(routeDbId)}
+        return route
+      `)
+      const routeDtos = yield cursor.map(routeDto)
+      return flatten(routeDtos.map(route => props.map(prop => ({
+        path: [routes, byId, route.id, prop],
+        value: defaultTo(route[prop], {$type: 'error', value: 'field does not exist'})
+      }))))
+    })
   }
 ]
 
 export default express.Router()
-
   .use(bodyParser.urlencoded({extended: false}))
-
   .use('/', falcor(() => new FalcorRouter(routes)))
 
 
@@ -68,6 +123,22 @@ function stationDto ({stop_id, stop_name, stop_lat, stop_lon}) {
     name: stop_name,
     latitude: stop_lat,
     longitude: stop_lon
+  }
+}
+
+function routeDbId (routeDtoId) {
+  return routeDtoId + '-0'
+}
+
+function routeDtoId (routeDbId) {
+  return routeDbId.split('-')[0]
+}
+
+function routeDto ({route_id, route_short_name, route_long_name}) {
+  return {
+    id: routeDtoId(route_id),
+    shortName: String(route_short_name),
+    longName: route_long_name
   }
 }
 

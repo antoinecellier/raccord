51571cbfcb63a39f43f3a88488dbb3a7a76f78d0
Hugo Wood <hx4.5@free.fr>
2016-09-21 23:13:28 +0200

    feat: run falcor request button

--- client/src/FalcorPanel/index.js
+++ client/src/FalcorPanel/index.js
@@ -3,13 +3,18 @@ import AceEditor from 'react-ace';
 import 'brace/mode/javascript';
 import 'brace/theme/github';
 
+import falcor from 'falcor/dist/falcor.all'
+
 export default class FalcorPanel extends Component {
   constructor(props) {
     super(props)
+    const model = new falcor.Model({source: new falcor.HttpDataSource('https://localhost:7081/falcor')})
+
     this.state = {
       request: ''
     }
     this.handleRequest = request => this.setState({ request });
+    this.handleExecute = () => model.get(...JSON.parse(this.state.request)).then(console.log.bind(console))
   }
 
   render() {
@@ -26,6 +31,7 @@ export default class FalcorPanel extends Component {
           value={this.state.request}
           editorProps={{$blockScrolling: true}}
         />
+        <button className="btn" onClick={this.handleExecute}>Get</button>
       </div>
     );
   }

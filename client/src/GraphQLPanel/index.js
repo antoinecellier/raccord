13dffdcfa2665b1809e2bda9025995326bab0ae6
antoinecellier <antoine08.cellier@hotmail.fr>
2016-09-26 21:51:36 +0200

    feature: add graphQLEditor

--- client/src/GraphQLPanel/index.js
+++ client/src/GraphQLPanel/index.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
+import './graphiql.css';
 
 export default class GraphQLPanel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fetcher: (graphQLParams) => {
+        const graphQLquery = this.state.query ? {query: this.state.query, variables: "{}", OperationName: null} : graphQLParams
+        return fetch('http://127.0.0.1:7080/graphql', {
+          method: 'post',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(graphQLquery),
+        }).then(response => {
+         const rep = response.json();
+         this.state.displayResponse(rep);
+         return rep;
+       })
+      },
+      onEditQuery: query => {
+        this.setState({query})
+        // TODO: Execute query translate function
+      },
+      query: null,
+      queryResult: '',
+      displayResponse: response => {
+        response.then(rep => this.setState({queryResult: JSON.stringify(rep.data)}));
+      }
+    };
+  }
+
   render () {
     return (
       <div className="col-md-6">
-        GraphQL
+        <GraphiQL fetcher={this.state.fetcher} onEditQuery={this.state.onEditQuery} />
+        <button className="btn btn-primary btn-block" onClick={this.state.fetcher}>Fire GraphQL!</button>
+        <pre>
+          {this.state.queryResult}
+        </pre>
       </div>
     )
   }

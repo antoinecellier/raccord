21c3ddb3cc8c06f663ff4ff4bf48163db99347e7
antoinecellier <antoine08.cellier@hotmail.fr>
2016-10-25 15:08:09 +0200

    maj du schéma pour la requete du 1er besoin

--- server/graphql/types/route.js
+++ server/graphql/types/route.js
@@ -1,11 +1,13 @@
 import { GraphQLString, GraphQLObjectType, GraphQLNonNull, GraphQLFloat, GraphQLEnumType } from 'graphql'
-
+import db, {aql} from '../../db'
 
 export const routeType = new GraphQLObjectType({
     name: 'Route',
     fields: () => ({
       route_id: { type: new GraphQLNonNull(GraphQLString) },
-      route_short_name: { type: new GraphQLNonNull(GraphQLString) },
-      route_long_name: { type: new GraphQLNonNull(GraphQLString) }
+      label: {
+        type: new GraphQLNonNull(GraphQLString),
+        resolve: ({ route_long_name }) => route_long_name
+      }
     })
 })

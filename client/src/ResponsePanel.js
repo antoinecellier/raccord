1b9e41ab7b4da8b32f1f1e691adfd8190225880f
Hugo Wood <hx4.5@free.fr>
2016-10-27 19:14:14 +0200

    feat: monospace font for response panel

--- client/src/ResponsePanel.js
+++ client/src/ResponsePanel.js
@@ -4,7 +4,7 @@ export default class ResponsePanel extends Component {
   render () {
     return (
       <div style={{flex: 1}}>
-        <textarea value={JSON.stringify(this.props.content, null, 2)} readOnly className="form-control" rows="48"/>
+        <textarea value={JSON.stringify(this.props.content, null, 2)} readOnly className="form-control" rows="48" style={{fontFamily: 'monospace'}} />
       </div>
     )
   }

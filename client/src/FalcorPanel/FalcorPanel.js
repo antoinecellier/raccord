6a5a7725cd47bc2a2981fa9ddd6e3d3456d5e470
Hugo Wood <hx4.5@free.fr>
2016-11-02 10:22:54 +0100

    fix: rewrote Falcor Panel component (fixes #27) (#34)
    * fix: #27
    
    * refactor: cleaner FalcorPanel component

---
+++ client/src/FalcorPanel/FalcorPanel.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import JsonEditor from './JsonEditor'
+
+export default class FalcorPanel extends Component {
+  handleRequestChange (newRequest) {
+    this.props.onRequestChange(newRequest)
+  }
+
+  handleFireRequest () {
+    this.props.model.get(...this.props.request).then(this.props.onResponse)
+  }
+
+  render () {
+    return (
+      <div style={{display: 'flex', flexDirection: 'column', flex: 1}}>
+        <JsonEditor value={this.props.request} onChange={json => this.handleRequestChange(json)} />
+        <button className="btn btn-primary btn-block" onClick={() => this.handleFireRequest()}>Fire Falcor!</button>
+      </div>
+    )
+  }
+}

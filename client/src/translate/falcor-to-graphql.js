a6a6d4f777ba8005cff1e274d81b5ba835d78fb5
Hugo Wood <hx4.5@free.fr>
2016-11-07 22:26:16 +0100

    feat: falcor path syntax support in falcor-to-graphql translation

--- client/src/translate/falcor-to-graphql.js
+++ client/src/translate/falcor-to-graphql.js
@@ -1,6 +1,7 @@
 import {parse} from 'graphql/language/parser'
 import { print } from 'graphql/language/printer'
 import _ from 'lodash'
+import falcorPathSyntax from 'falcor-path-syntax'
 
 // [["stations", "search", "co", {"from":0, "length": 10},[ "id", "label"]]]
 // [["stations", "search", "co", {"from":0, "length": 10}, "routes", {"length": 20}, "label"]],
@@ -9,7 +10,9 @@ export default function translate (inputFalcor) {
   const falcorRequest = inputFalcor
   let graphQlRequest = '{'
   if (falcorRequest[0]) {
-    const request = falcorRequest[0]
+    const request = typeof falcorRequest[0] === 'string'
+      ? falcorPathSyntax(falcorRequest[0])
+      : falcorRequest[0]
 
     graphQlRequest = graphQlRequest.concat(request[0])
 
@@ -17,6 +20,10 @@ export default function translate (inputFalcor) {
 
     if (_.isObject(request[1])) {
       graphQlRequest = graphQlRequest.concat('(')
+      if (request[1].to) {
+        request[1].length = request[1].to + 1
+        delete request[1].to
+      }
       _.forIn(request[1], (v, k) => {
         graphQlRequest = graphQlRequest.concat(`${k}:${v},`)
       })
@@ -24,7 +31,15 @@ export default function translate (inputFalcor) {
     } else if (_.isObject(request[3])) {
       graphQlRequest = graphQlRequest.concat('(')
       graphQlRequest = graphQlRequest.concat(`${request[1]}:"${request[2]}", `)
-      _.forIn(request[indexOfPagination], (v, k) => {
+      if (request[3].to) {
+        request[3].length = request[3].to + 1
+        delete request[3].to
+      }
+      if (request[1].to) {
+        request[1].length = request[1].to + 1
+        delete request[1].to
+      }
+      _.forIn(request[3], (v, k) => {
         graphQlRequest = graphQlRequest.concat(`${k}:${v}, `)
       })
       graphQlRequest = graphQlRequest.concat('){')
@@ -32,7 +47,11 @@ export default function translate (inputFalcor) {
       graphQlRequest = graphQlRequest.concat('(')
       graphQlRequest = graphQlRequest.concat(`${request[1]}:"${request[2]}", `)
       graphQlRequest = graphQlRequest.concat(`${request[3]}:"${request[4]}", `)
-      _.forIn(request[indexOfPagination], (v, k) => {
+      if (request[5].to) {
+        request[5].length = request[5].to + 1
+        delete request[5].to
+      }
+      _.forIn(request[5], (v, k) => {
         graphQlRequest = graphQlRequest.concat(`${k}:${v}, `)
       })
       graphQlRequest = graphQlRequest.concat('){')

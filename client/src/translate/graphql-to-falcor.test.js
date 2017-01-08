1df586ba72da4125993f5184cbea2179630aa38c
Hugo Wood <hx4.5@free.fr>
2017-01-08 11:44:55 +0100

    style: fixed lint errors in client tests

--- client/src/translate/graphql-to-falcor.test.js
+++ client/src/translate/graphql-to-falcor.test.js
@@ -29,54 +29,74 @@ test('simple field', t => {
 
 test('simple field with one argument', t => {
   assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, arguments: [
-      {name: {value: 'arg'}, value: {value: 'argValue'}}
-    ]}),
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      arguments: [
+        {name: {value: 'arg'}, value: {value: 'argValue'}}
+      ]
+    }),
     [['field', 'arg', 'argValue']])
   t.end()
 })
 
 test('simple field with several argument', t => {
   assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, arguments: [
-      {name: {value: 'arg'}, value: {value: 'argValue'}},
-      {name: {value: 'arg'}, value: {value: 'argValue'}},
-      {name: {value: 'arg'}, value: {value: 'argValue'}}
-    ]}),
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      arguments: [
+        {name: {value: 'arg'}, value: {value: 'argValue'}},
+        {name: {value: 'arg'}, value: {value: 'argValue'}},
+        {name: {value: 'arg'}, value: {value: 'argValue'}}
+      ]
+    }),
     [['field', 'arg', 'argValue', 'arg', 'argValue', 'arg', 'argValue']])
   t.end()
 })
 
 test('simple field with one range argument', t => {
   assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, arguments: [
-      {name: {value: 'from'}, value: {value: 0}},
-      {name: {value: 'length'}, value: {value: 10}}
-    ]}),
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      arguments: [
+        {name: {value: 'from'}, value: {value: 0}},
+        {name: {value: 'length'}, value: {value: 10}}
+      ]
+    }),
     [['field', {from: 0, length: 10}]])
   t.end()
 })
 
 test('simple field with one range argument and one normal argument', t => {
   assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, arguments: [
-      {name: {value: 'from'}, value: {value: 0}},
-      {name: {value: 'length'}, value: {value: 10}},
-      {name: {value: 'arg'}, value: {value: 'argValue'}}
-    ]}),
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      arguments: [
+        {name: {value: 'from'}, value: {value: 0}},
+        {name: {value: 'length'}, value: {value: 10}},
+        {name: {value: 'arg'}, value: {value: 'argValue'}}
+      ]
+    }),
     [['field', 'arg', 'argValue', {from: 0, length: 10}]])
   t.end()
 })
 
 test('fields with two selections', t => {
   assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, selectionSet: {
-      kind: 'SelectionSet',
-      selections: [
-        {kind: 'Field', name: {value: 'child1'}},
-        {kind: 'Field', name: {value: 'child2'}}
-      ]
-    }}),
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {kind: 'Field', name: {value: 'child1'}},
+          {kind: 'Field', name: {value: 'child2'}}
+        ]
+      }
+    }),
     [['field', 'child1'], ['field', 'child2']]
   )
   t.end()
@@ -84,17 +104,24 @@ test('fields with two selections', t => {
 
 test('fields with nested selection', t => {
   assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, selectionSet: {
-      kind: 'SelectionSet',
-      selections: [
-        {kind: 'Field', name: {value: 'child1'}, selectionSet: {
-          kind: 'SelectionSet',
-          selections: [
-            {kind: 'Field', name: {value: 'child2'}}
-          ]
-        }}
-      ]
-    }}),
+    translateNode({kind: 'Field',
+      name: {value: 'field'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: {value: 'child1'},
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {kind: 'Field', name: {value: 'child2'}}
+              ]
+            }
+          }
+        ]
+      }
+    }),
     [['field', 'child1', 'child2']]
   )
   t.end()
@@ -102,18 +129,26 @@ test('fields with nested selection', t => {
 
 test('fields with a nested selection and a simple selection before', t => {
   assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, selectionSet: {
-      kind: 'SelectionSet',
-      selections: [
-        {kind: 'Field', name: {value: 'child3'}},
-        {kind: 'Field', name: {value: 'child1'}, selectionSet: {
-          kind: 'SelectionSet',
-          selections: [
-            {kind: 'Field', name: {value: 'child2'}}
-          ]
-        }}
-      ]
-    }}),
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {kind: 'Field', name: {value: 'child3'}},
+          {
+            kind: 'Field',
+            name: {value: 'child1'},
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {kind: 'Field', name: {value: 'child2'}}
+              ]
+            }
+          }
+        ]
+      }
+    }),
     [['field', 'child3'], ['field', 'child1', 'child2']]
   )
   t.end()
@@ -121,18 +156,26 @@ test('fields with a nested selection and a simple selection before', t => {
 
 test('fields with a nested selection and a simple selection after', t => {
   assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, selectionSet: {
-      kind: 'SelectionSet',
-      selections: [
-        {kind: 'Field', name: {value: 'child1'}, selectionSet: {
-          kind: 'SelectionSet',
-          selections: [
-            {kind: 'Field', name: {value: 'child2'}}
-          ]
-        }},
-        {kind: 'Field', name: {value: 'child3'}}
-      ]
-    }}),
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: {value: 'child1'},
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {kind: 'Field', name: {value: 'child2'}}
+              ]
+            }
+          },
+          {kind: 'Field', name: {value: 'child3'}}
+        ]
+      }
+    }),
     [['field', 'child1', 'child2'], ['field', 'child3']]
   )
   t.end()

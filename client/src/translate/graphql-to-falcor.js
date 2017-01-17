cdcf2b6ec1adad9b813bf25500f284900449b62c
Hugo Wood <hx4.5@free.fr>
2017-01-17 21:31:00 +0100

    chore: made client-side test harness functional (#43)
    * chore: made client-side test harness functional
    
    * refactor: switch tests to es6 import syntax
    
    * style: fixed lint errors in client tests
    
    * fix: assertion using tape and not assert
    
    Makes for better errors and correct reporting

--- client/src/translate/graphql-to-falcor.js
+++ client/src/translate/graphql-to-falcor.js
@@ -2,34 +2,32 @@ import {parse} from 'graphql/language/parser'
 import _ from 'lodash'
 
 export default function translate (inputGraphQl) {
-  const parsedGraphQl = parse(inputGraphQl)
+  return translateNode(parse(inputGraphQl))
+}
 
-  function translateNode (node) {
-    switch (node.kind) {
-      case 'Document': return translateNode(node.definitions[0])
-      case 'OperationDefinition': return translateNode(node.selectionSet)
-      case 'Field': return visitField(node)
-      case 'SelectionSet': return _.flatMap(node.selections, translateNode)
-      default: throw new Error('unknown kind: ' + node.kind)
-    }
-
-    function visitField (node) {
-      const subtree = node.selectionSet ? translateNode(node.selectionSet) : [[]]
-      const range = buildRange(node.arguments)
-      const args = _.flatMap(node.arguments, arg => [arg.name.value, arg.value.value])
-      return _.map(subtree, path => [node.name.value, ...args, ...range, ...path])
-    }
-
-    function buildRange (args) {
-      const [fromArg] = _.remove(args, {name: {value: 'from'}})
-      const [lengthArg] = _.remove(args, {name: {value: 'length'}})
-      if (!fromArg && !lengthArg) return []
-      const range = {}
-      if (fromArg) range.from = Number(fromArg.value.value)
-      if (lengthArg) range.length = Number(lengthArg.value.value)
-      return [range]
-    }
+export function translateNode (node) {
+  switch (node.kind) {
+    case 'Document': return translateNode(node.definitions[0])
+    case 'OperationDefinition': return translateNode(node.selectionSet)
+    case 'Field': return visitField(node)
+    case 'SelectionSet': return _.flatMap(node.selections, translateNode)
+    default: throw new Error('unknown kind: ' + node.kind)
   }
+}
 
-  return translateNode(parsedGraphQl)
+function visitField (node) {
+  const subtree = node.selectionSet ? translateNode(node.selectionSet) : [[]]
+  const range = buildRange(node.arguments)
+  const args = _.flatMap(node.arguments, arg => [arg.name.value, arg.value.value])
+  return _.map(subtree, path => [node.name.value, ...args, ...range, ...path])
+}
+
+function buildRange (args) {
+  const [fromArg] = _.remove(args, {name: {value: 'from'}})
+  const [lengthArg] = _.remove(args, {name: {value: 'length'}})
+  if (!fromArg && !lengthArg) return []
+  const range = {}
+  if (fromArg) range.from = Number(fromArg.value.value)
+  if (lengthArg) range.length = Number(lengthArg.value.value)
+  return [range]
 }
--- client/src/translate/graphql-to-falcor.test.js
+++ client/src/translate/graphql-to-falcor.test.js
@@ -1,10 +1,8 @@
-const test = require('tape')
-const assert = require('assert')
-
-// translateNode is from graphql-to-falcor.js
+import test from 'tape'
+import {translateNode} from './graphql-to-falcor'
 
 test('simple document', t => {
-  assert.deepEqual(
+  t.deepEqual(
     translateNode({
       kind: 'Document',
       definitions: [
@@ -22,118 +20,161 @@ test('simple document', t => {
 })
 
 test('simple field', t => {
-  assert.deepEqual(
+  t.deepEqual(
     translateNode({kind: 'Field', name: {value: 'field'}}),
     [['field']])
   t.end()
 })
 
 test('simple field with one argument', t => {
-  assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, arguments: [
-      {name: {value: 'arg'}, value: {value: 'argValue'}}
-    ]}),
+  t.deepEqual(
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      arguments: [
+        {name: {value: 'arg'}, value: {value: 'argValue'}}
+      ]
+    }),
     [['field', 'arg', 'argValue']])
   t.end()
 })
 
 test('simple field with several argument', t => {
-  assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, arguments: [
-      {name: {value: 'arg'}, value: {value: 'argValue'}},
-      {name: {value: 'arg'}, value: {value: 'argValue'}},
-      {name: {value: 'arg'}, value: {value: 'argValue'}}
-    ]}),
+  t.deepEqual(
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      arguments: [
+        {name: {value: 'arg'}, value: {value: 'argValue'}},
+        {name: {value: 'arg'}, value: {value: 'argValue'}},
+        {name: {value: 'arg'}, value: {value: 'argValue'}}
+      ]
+    }),
     [['field', 'arg', 'argValue', 'arg', 'argValue', 'arg', 'argValue']])
   t.end()
 })
 
 test('simple field with one range argument', t => {
-  assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, arguments: [
-      {name: {value: 'from'}, value: {value: 0}},
-      {name: {value: 'length'}, value: {value: 10}}
-    ]}),
+  t.deepEqual(
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      arguments: [
+        {name: {value: 'from'}, value: {value: 0}},
+        {name: {value: 'length'}, value: {value: 10}}
+      ]
+    }),
     [['field', {from: 0, length: 10}]])
   t.end()
 })
 
 test('simple field with one range argument and one normal argument', t => {
-  assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, arguments: [
-      {name: {value: 'from'}, value: {value: 0}},
-      {name: {value: 'length'}, value: {value: 10}},
-      {name: {value: 'arg'}, value: {value: 'argValue'}}
-    ]}),
+  t.deepEqual(
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      arguments: [
+        {name: {value: 'from'}, value: {value: 0}},
+        {name: {value: 'length'}, value: {value: 10}},
+        {name: {value: 'arg'}, value: {value: 'argValue'}}
+      ]
+    }),
     [['field', 'arg', 'argValue', {from: 0, length: 10}]])
   t.end()
 })
 
 test('fields with two selections', t => {
-  assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, selectionSet: {
-      kind: 'SelectionSet',
-      selections: [
-        {kind: 'Field', name: {value: 'child1'}},
-        {kind: 'Field', name: {value: 'child2'}}
-      ]
-    }}),
+  t.deepEqual(
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {kind: 'Field', name: {value: 'child1'}},
+          {kind: 'Field', name: {value: 'child2'}}
+        ]
+      }
+    }),
     [['field', 'child1'], ['field', 'child2']]
   )
   t.end()
 })
 
 test('fields with nested selection', t => {
-  assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, selectionSet: {
-      kind: 'SelectionSet',
-      selections: [
-        {kind: 'Field', name: {value: 'child1'}, selectionSet: {
-          kind: 'SelectionSet',
-          selections: [
-            {kind: 'Field', name: {value: 'child2'}}
-          ]
-        }}
-      ]
-    }}),
+  t.deepEqual(
+    translateNode({kind: 'Field',
+      name: {value: 'field'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: {value: 'child1'},
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {kind: 'Field', name: {value: 'child2'}}
+              ]
+            }
+          }
+        ]
+      }
+    }),
     [['field', 'child1', 'child2']]
   )
   t.end()
 })
 
 test('fields with a nested selection and a simple selection before', t => {
-  assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, selectionSet: {
-      kind: 'SelectionSet',
-      selections: [
-        {kind: 'Field', name: {value: 'child3'}},
-        {kind: 'Field', name: {value: 'child1'}, selectionSet: {
-          kind: 'SelectionSet',
-          selections: [
-            {kind: 'Field', name: {value: 'child2'}}
-          ]
-        }}
-      ]
-    }}),
+  t.deepEqual(
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {kind: 'Field', name: {value: 'child3'}},
+          {
+            kind: 'Field',
+            name: {value: 'child1'},
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {kind: 'Field', name: {value: 'child2'}}
+              ]
+            }
+          }
+        ]
+      }
+    }),
     [['field', 'child3'], ['field', 'child1', 'child2']]
   )
   t.end()
 })
 
 test('fields with a nested selection and a simple selection after', t => {
-  assert.deepEqual(
-    translateNode({kind: 'Field', name: {value: 'field'}, selectionSet: {
-      kind: 'SelectionSet',
-      selections: [
-        {kind: 'Field', name: {value: 'child1'}, selectionSet: {
-          kind: 'SelectionSet',
-          selections: [
-            {kind: 'Field', name: {value: 'child2'}}
-          ]
-        }},
-        {kind: 'Field', name: {value: 'child3'}}
-      ]
-    }}),
+  t.deepEqual(
+    translateNode({
+      kind: 'Field',
+      name: {value: 'field'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: {value: 'child1'},
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {kind: 'Field', name: {value: 'child2'}}
+              ]
+            }
+          },
+          {kind: 'Field', name: {value: 'child3'}}
+        ]
+      }
+    }),
     [['field', 'child1', 'child2'], ['field', 'child3']]
   )
   t.end()

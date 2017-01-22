2ce5a68ab47cf489fb71c4ab32c9a762850eeae4
Hugo Wood <hx4.5@free.fr>
2017-01-22 21:56:42 +0100

    refactor(falcor-to-graphql): groupArgs preprocesses the path to convert ranges to args
    Instead of doing it at the same time as detecting args.

--- client/src/translate/falcor-to-graphql.js
+++ client/src/translate/falcor-to-graphql.js
@@ -159,42 +159,43 @@ function translateArgAwarePath (path) {
  * @returns {ArgAwarePath}
  */
 export function groupArgs (path, type, schema) {
-  // This function is recursive and one pass of it only moves forward one field into the model.
-  // It attempts to capture the field (which name is the first segment of the given path) and its arguments (if there are any).
-  // As soon as it encounters a path segment that is not an argument to the first field, it assumes it is the next field down the model and recurses.
-  if (path.length === 0) return []
-  let rangeWasExpanded = false // CAREFUL: mutated by inner function rangeToArgs
-  // We are sure the first segment is a field into the given type (that's the function contract).
-  // Everything after that might be arguments to this field, or that might be other fields.
-  // We have to scan the segments until we find the first segment that is not an argument name (according to the schema).
-  const [field, ...maybeArgs] = path
-  const fieldSchema = schema && schema.getType(type).getFields()[field] || {args: {}, type: {name: type}}
-  const args = _(maybeArgs)
-    .flatMap(maybeArg => _.isPlainObject(maybeArg) ? rangeToArgs(maybeArg) : [maybeArg])
-    .chunk(2)
-    .takeWhile(([name]) => _.some(fieldSchema.args, {name}))
-    .fromPairs()
-    .mapValues((value, name) => ({value: _.isNil(value) ? '' : value, type: typeOf(_.find(fieldSchema.args, {name}))}))
-    .value()
-  // So now we have the arguments but we have lost the information of how many segments they take up in the original path.
-  // We have to re-compute this to strip those segment from the original path and continue to process the rest of the path.
-  // Beware of the nasty rangeWasExpanded which is true when a Falcor range object was expanded to normal-looking arguments (that is, rangeToArgs was called).
-  const numberOfPathSegmentTakenByArgs = (Object.keys(args).length * 2) - (3 * +rangeWasExpanded)
-  const restOfPath = _.drop(maybeArgs, numberOfPathSegmentTakenByArgs)
-  const typeOfRestOfPath = typeOf(fieldSchema)
-  return [{field, args}, ...groupArgs(restOfPath, typeOfRestOfPath, schema)]
-
-  function typeOf ({type}) {
-    return getNamedType(type).name
-  }
+  const pathWithRangesAsArgs = _.flatMap(path, segment => _.isPlainObject(segment) ? rangeToArgs(segment) : [segment])
+  return actuallyGroupArgs(pathWithRangesAsArgs, type, schema)
 
   /**
    * Transforms a Falcor range object to an array of 4 elements so it looks like 2 argument pairs.
    */
   function rangeToArgs ({from = 0, to = 1, length}) {
-    rangeWasExpanded = true // this will trigger proper calculation of numberOfPathSegmentTakenByArgs
     if (!length) length = to + 1
     return ['from', from, 'length', length]
+  }
+
+  function actuallyGroupArgs (path, type, schema) {
+    // This function is recursive and one pass of it only moves forward one field into the model.
+    // It attempts to capture the field (which name is the first segment of the given path) and its arguments (if there are any).
+    // As soon as it encounters a path segment that is not an argument to the first field, it assumes it is the next field down the model and recurses.
+    if (path.length === 0) return []
+    // We are sure the first segment is a field into the given type (that's the function contract).
+    // Everything after that might be arguments to this field, or that might be other fields.
+    // We have to scan the segments until we find the first segment that is not an argument name (according to the schema).
+    const [field, ...maybeArgs] = path
+    const fieldSchema = schema && schema.getType(type).getFields()[field] || {args: {}, type: {name: type}}
+    const args = _(maybeArgs)
+      .chunk(2)
+      .takeWhile(([name]) => _.some(fieldSchema.args, {name}))
+      .fromPairs()
+      .mapValues((value, name) => ({value: _.isNil(value) ? '' : value, type: typeOf(_.find(fieldSchema.args, {name}))}))
+      .value()
+    // So now we have the arguments but we have lost the information of how many segments they take up in the original path.
+    // We have to re-compute this to strip those segment from the original path and continue to process the rest of the path.
+    const numberOfPathSegmentTakenByArgs = Object.keys(args).length * 2
+    const restOfPath = _.drop(maybeArgs, numberOfPathSegmentTakenByArgs)
+    const typeOfRestOfPath = typeOf(fieldSchema)
+    return [{field, args}, ...groupArgs(restOfPath, typeOfRestOfPath, schema)]
+
+    function typeOf ({type}) {
+      return getNamedType(type).name
+    }
   }
 }
 
